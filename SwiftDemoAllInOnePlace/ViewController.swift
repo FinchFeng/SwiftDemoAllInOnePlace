d9ffdbe73dc8c50ca0a3bdf60fe6b7aee4675bd2
FinchFeng <329650437@qq.com>
2019-06-06 19:00:34 +0800

    Initial Commit

---
+++ SwiftDemoAllInOnePlace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftDemoAllInOnePlace
+//
+//  Created by 冯奕琦 on 2019/6/6.
+//  Copyright © 2019 冯奕琦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
